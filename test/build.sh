32727c0972b519f693ef28d80234ea531a6fe0e8
yangn0 <yangn0@qq.com>
2023-12-30 20:56:04 +0800

    🌈 style: change ~ to $HOME in .sh

--- test/build.sh
+++ test/build.sh
@@ -2,14 +2,14 @@
 export bsp=raspberrypi4b
 export rtemsversion=rtems-aarch64
 
-export PATH=/home/yangn0/devel/$rtemsversion/rtems/6/bin:$PATH
+export PATH=$HOME/devel/$rtemsversion/rtems/6/bin:$PATH
 
-cd /home/yangn0/devel/app/test
-rm -rf /home/yangn0/devel/app/test/build
+cd $HOME/devel/app/test
+rm -rf $HOME/devel/app/test/build
 
 ./waf configure --rtems=$HOME/devel/$rtemsversion/rtems/6 --rtems-bsp=aarch64/$bsp
 ./waf 
 
-mv /home/yangn0/devel/app/test/build/aarch64-rtems6-$bsp/test.exe /home/yangn0/devel/app/test/
-rm  -rf /home/yangn0/devel/app/test/build
-aarch64-rtems6-objcopy -O binary /home/yangn0/devel/app/test/test.exe kernel8.img
+mv $HOME/devel/app/test/build/aarch64-rtems6-$bsp/test.exe $HOME/devel/app/test/
+rm  -rf $HOME/devel/app/test/build
+aarch64-rtems6-objcopy -O binary $HOME/devel/app/test/test.exe kernel8.img
--- test/build_make.sh
+++ test/build_make.sh
@@ -2,17 +2,17 @@
 export bsp=raspberrypi4b
 export rtemsversion=rtems-aarch64
 
-# export PATH=/home/yangn0/devel/rtems-aarch64-utk/rtems/6/bin:$PATH
-export PATH=/home/yangn0/devel/$rtemsversion/rtems/6/bin:$PATH
-export RTEMS_MAKEFILE_PATH=/home/yangn0/devel/$rtemsversion/rtems/6/aarch64-rtems6/$bsp
-export PROJECT_ROOT=/home/yangn0/devel/$rtemsversion/src/rtems
+# export PATH=$HOME/devel/rtems-aarch64-utk/rtems/6/bin:$PATH
+export PATH=$HOME/devel/$rtemsversion/rtems/6/bin:$PATH
+export RTEMS_MAKEFILE_PATH=$HOME/devel/$rtemsversion/rtems/6/aarch64-rtems6/$bsp
+export PROJECT_ROOT=$HOME/devel/$rtemsversion/src/rtems
 
-cd /home/yangn0/devel/app/test
+cd $HOME/devel/app/test
 make clean
 make 
 
-mv /home/yangn0/devel/app/test/o-optimize/test.exe /home/yangn0/devel/app/test/
+mv $HOME/devel/app/test/o-optimize/test.exe $HOME/devel/app/test/
 
-rm -rf /home/yangn0/devel/app/test/o-optimize/
+rm -rf $HOME/devel/app/test/o-optimize/
 
-aarch64-rtems6-objcopy -O binary /home/yangn0/devel/app/test/test.exe kernel8.img
+aarch64-rtems6-objcopy -O binary $HOME/devel/app/test/test.exe kernel8.img

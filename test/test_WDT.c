1015481a3b2da59eedb89197a4e1e388fdab65d0
Ning Yang <yangn0@qq.com>
2024-07-20 00:04:36 +0800

    add WDT test

---
+++ test/test_WDT.c
@@ -0,0 +1,52 @@
+#include <rtems.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <bsp/watchdog.h>
+
+rtems_task Init(
+    rtems_task_argument ignored)
+{
+  puts("hello");
+
+  raspberrypi_watchdog_init();
+
+  raspberrypi_watchdog_reload(0, 10);
+  printf("%d\n", raspberrypi_watchdog_get_timeleft());
+  raspberrypi_watchdog_stop(0);
+  sleep(3);
+  printf("%d\n", raspberrypi_watchdog_get_timeleft());
+
+  raspberrypi_watchdog_reload(0, 10);
+  sleep(3);
+  printf("%d\n", raspberrypi_watchdog_get_timeleft());
+  puts("end");
+
+  // raspberrypi_watchdog_system_reset();
+
+  rtems_shutdown_executive(0);
+}
+
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_UNLIMITED_OBJECTS
+#define CONFIGURE_UNIFIED_WORK_AREAS
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#define CONFIGURE_FILESYSTEM_IMFS
+
+#define CONFIGURE_APPLICATION_NEEDS_LIBBLOCK
+#define CONFIGURE_MAXIMUM_TASKS 20
+#define CONFIGURE_MAXIMUM_SEMAPHORES 20
+#define CONFIGURE_MAXIMUM_MESSAGE_QUEUES 20
+#define CONFIGURE_MAXIMUM_FILE_DESCRIPTORS 20
+#define CONFIGURE_STACK_CHECKER_ENABLED
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+#define CONFIGURE_EXTRA_TASK_STACKS (6 * RTEMS_MINIMUM_STACK_SIZE)
+#define CONFIGURE_MALLOC_STATISTICS
+#define CONFIGURE_UNIFIED_WORK_AREAS
+
+#include <rtems/confdefs.h>
